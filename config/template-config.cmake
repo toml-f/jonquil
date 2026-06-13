ee91f2f1f6c03301bd714b16ca2da7bbfa75ffad
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-06-13 10:05:35 +0200

    Add CMake export from meson (#30)

--- config/template-config.cmake
+++ config/template-config.cmake
@@ -1,13 +1,13 @@
 @PACKAGE_INIT@
 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+set("JONQUIL_USE_TOMLF" @JONQUIL_USE_TOMLF@)
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 
   include(CMakeFindDependencyMacro)
 
-  if(NOT TARGET "toml-f::toml-f")
+  if(NOT TARGET "toml-f::toml-f" AND JONQUIL_USE_TOMLF)
     find_dependency("toml-f")
   endif()
 endif()
